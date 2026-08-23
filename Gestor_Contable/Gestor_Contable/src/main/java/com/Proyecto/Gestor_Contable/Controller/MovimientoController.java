75dce6d2e4f01409fd698ae730f61c238b336679
jonathan <jmanjarres238@gmail.com>
2026-08-23 18:39:25 -0500

    probando rama y push

--- Gestor_Contable/Gestor_Contable/src/main/java/com/Proyecto/Gestor_Contable/Controller/MovimientoController.java
+++ Gestor_Contable/Gestor_Contable/src/main/java/com/Proyecto/Gestor_Contable/Controller/MovimientoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 /*
-probando
+probando otra vez la rama y los push
  */
 @RestController
 @RequestMapping("/api/movimiento")
